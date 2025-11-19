b3de8e1c3ffba2c881844f747f7e71fac02313c6
Fahim Rahman <140365290+FahimRahman10@users.noreply.github.com>
2025-11-20 00:54:36 +0600

    Time: 2 ms (65.59%), Space: 146.6 MB (100%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.dart
+++ 0002-add-two-numbers/0002-add-two-numbers.dart
@@ -1,26 +1,16 @@
-/**
- * Definition for singly-linked list.
- * class ListNode {
- *   int val;
- *   ListNode? next;
- *   ListNode([this.val = 0, this.next]);
- * }
- */
 class Solution {
-  ListNode? addTwoNumbers(ListNode? l1, ListNode? l2) {
-    ListNode dummy = ListNode(0);
-    ListNode current = dummy;
-    int carry = 0;
-    while (l1 != null || l2 != null || carry != 0) {
-        int x = (l1 != null) ? l1.val : 0;
-        int y = (l2 != null) ? l2.val : 0;
-        int sum = x + y + carry;
-        carry = sum ~/ 10;
-        current.next = ListNode(sum % 10);
-        current = current.next!;
-        if (l1 != null) l1 = l1.next;
-        if (l2 != null) l2 = l2.next;
-    }
-    return dummy.next;
+  ListNode? addTwoNumbers(ListNode? l1, ListNode? l2, [int carry = 0]){
+    if (l1 == null && l2 == null && carry == 0) return null;
+    int x = l1?.val ?? 0;
+    int y = l2?.val ?? 0;
+    int sum = x + y + carry;
+
+    ListNode node = ListNode(sum % 10);
+    node.next = addTwoNumbers(
+      l1?.next,
+      l2?.next,
+      sum ~/ 10,
+    );
+    return node;
   }
 }
