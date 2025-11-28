bfdddf20aed4906133c3ec84559c1ead41c5adbd
Fahim Rahman <140365290+FahimRahman10@users.noreply.github.com>
2025-11-28 20:40:59 +0600

    Time: 4 ms (45.04%), Space: 8.9 MB (76.61%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -1,8 +1,10 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        if (n <= 0) return false;
-        int po3 = 1162261467;
-        return po3 % n == 0;
+        if(n<=0) return false;
+        while(n%3==0){
+            n/=3;
+        }
+        return n==1;
     }
 };
